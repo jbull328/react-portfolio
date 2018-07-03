1ead01b2cfaf4d3872981f0d2125ff98bf48e5f8
John T. Bull <jackbull328@gmail.com>
2018-07-03 12:26:22 -0700

    Added some more info
    Added some new info about me.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,13 @@ class Landing extends Component {
     return (
       <div className="Landing">
         <h3>Welcome to my protfolio site.</h3>
-        <p>I had fun redesigning this recently with React and CSS Grid. Please check out my projects and reach out to me with any questions.
+        <p>My Name is John? I love working on code and getting into the weeds in projects, it makes me happy. 
+            I would rather live in a shack by the beach with a few items than have a huge house with many items.  
+            I’ve been working in the Tech industry since 2012 (so much fun) and it has been a great journy.  
+            I currently work full-time at Datapath, CA working on website front end and freelancing on projects as my limited time will allow.  
+            I am curous and keep learning and growing my developemnt skill everyday and am comfortable working with the whole stack. 
+            I worked hard for two years organizing monthly meetups for developers in our area and we were even featured as one of FreeCodeCamps top meetups.
+            <br/>I am a family man, I have two great kids and I love spending as much time with them as I can. I love creating things outside of code and it fules my fire, to cook, brew my own beer, and contribute in my community.<br/>
             <br/>Thanks, and enjoy.<br/>
           John
         </p>
