6298e40b073f91b3bd7f00bfd27dabb9af4c1389
jbull328 <jackbull328@gmail.com>
2018-08-15 20:42:00 -0700

    Added font loader and changed content

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,12 +6,11 @@ class Landing extends Component {
     return (
       <div className="Landing">
         <h3>Welcome to my protfolio site.</h3>
-        <p>My Name is John? I love working on code and getting into the weeds in projects, it makes me happy. 
-            I would rather live in a shack by the beach with a few items than have a huge house with many items.  
-            I’ve been working in the Tech industry since 2012 (so much fun) and it has been a great journy.  
-            I currently work full-time at Datapath, CA working on website front end and freelancing on projects as my limited time will allow.  
-            I am curous and keep learning and growing my developemnt skill everyday and am comfortable working with the whole stack. 
-            I worked hard for two years organizing monthly meetups for developers in our area and we were even featured as one of FreeCodeCamps top meetups.
+        <p>• 4+ years' professional experience with JavaScript technology on both front-end and back-end web application design and development.
+        <br/>• Proficiency in developing web applications with React UI, JQuery, Node, Express, AWS, API integrations, NoSQL Databases.
+        <br/>• In-depth experience in front-end web design and development using HTML5, CSS, Bootstrap, Javascript, jQuery, JSON, AJAX, React and Angular 2/4.
+        <br/>• Great team player with strong communication skills, problem-solving skills and ability to learn as well as adapting to new technology.
+        <br/>• Agile Methodology, with remote teams or in-house team
             <br/>I am a family man, I have two great kids and I love spending as much time with them as I can. I love creating things outside of code and it fules my fire, to cook, brew my own beer, and contribute in my community.<br/>
             <br/>Thanks, and enjoy.<br/>
           John
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Raleway:300,400', 'sans-serif']
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
