842ccebb5377c43fb076f9603492982058382960
jbull328 <jackbull328@gmail.com>
2018-09-26 22:43:05 -0700

    added mobile styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import defaultImage from '../img/default.jpg';
-import meImg from '../img/me_bw.JPG';
-import styled from 'styled-components';
-import Contact from './Contact';
+import React, { Component } from "react";
+import meImg from "../img/me_bw.JPG";
+import styled from "styled-components";
+import Contact from "./Contact";
 
 class About extends Component {
-  render () {
+  render() {
     return (
       <AboutWrapper>
         <AboutInfo>
-          <img src={meImg}/>
+          <img src={meImg} alt="John" />
           <Contact />
           <h2>The right mix of experience and creativity</h2>
-          <p>Creative problem solver with experience working with diverse, distributed teams. Fullstack projects built with Node.js, Express, Javascript, React, MongoDB, hosted on AWS ec2, and Heroku. 
-              Three years experience as a FrontEnd Developer at an agency, focusing on mobile styling and cross-browser optimization/testing.
+          <p>
+            Creative problem solver with experience working with diverse,
+            distributed teams. Fullstack projects built with Node.js, Express,
+            Javascript, React, MongoDB, hosted on AWS ec2, and Heroku. Three
+            years experience as a FrontEnd Developer at an agency, focusing on
+            mobile styling and cross-browser optimization/testing.
           </p>
         </AboutInfo>
       </AboutWrapper>
@@ -23,26 +26,25 @@ class About extends Component {
 
 export default About;
 
-const AboutWrapper = styled.div `
-`;
+const AboutWrapper = styled.div``;
 
-const AboutInfo = styled.div `
-    color: #fff;
-    text-align: left;
-    padding: 2rem 10%;
-    display: flex;
-    flex-direction: column;
-    line-height: 1.4;
-    img {
-      max-width: 254px;
-      height: auto;
-      flex-shrink: auto;
-    }
-    span {
-      font-size: 16px;
-      font-weight: bold;
-      color: #999;
-      text-transform: uppercase;
-      letter-spacing: .05em;
-    }
+const AboutInfo = styled.div`
+  color: #fff;
+  text-align: left;
+  padding: 2rem 10%;
+  display: flex;
+  flex-direction: column;
+  line-height: 1.4;
+  img {
+    max-width: 254px;
+    height: auto;
+    flex-shrink: auto;
+  }
+  span {
+    font-size: 16px;
+    font-weight: bold;
+    color: #999;
+    text-transform: uppercase;
+    letter-spacing: 0.05em;
+  }
 `;
