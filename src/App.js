81ffb75b6738ace199a501a80cce250625b3acef
jbull328 <jackbull328@gmail.com>
2019-04-19 14:36:16 -0700

    Adding current items to landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import ProjectsList from './components/ProjectsList';
-import styled from 'styled-components';
+
 import About from './components/About';
 import Landing from './components/Landing';
 
@@ -17,31 +12,34 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-        <header className="App-header container">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="brand container">
-            <h1 className="App-title">John Bull ~ Web Developer</h1>
-            <h3 className="App-title">Available for Hire</h3>
-          </div>
-         
-          <div className="links">
-            <Link to="/" component={Landing}>Home</Link>
-            <Link to="/about" component={About}>About</Link> 
-            <Link to="/Projects" component={ProjectsList}>Projects</Link>
-          </div>   
-        </header>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/Projects" component={ProjectsList} />
-          <Route path="/about" component={About} />
-        </Switch>
-          
+          <header className="App-header container">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="brand container">
+              <h1 className="App-title">John Bull ~ Web Developer</h1>
+              <h3 className="App-title">Available for Hire</h3>
+            </div>
+
+            <div className="links">
+              <Link to="/" component={Landing}>
+                Home
+              </Link>
+              <Link to="/about" component={About}>
+                About
+              </Link>
+              <Link to="/Projects" component={ProjectsList}>
+                Projects
+              </Link>
+            </div>
+          </header>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/Projects" component={ProjectsList} />
+            <Route path="/about" component={About} />
+          </Switch>
         </div>
       </Router>
-      
     );
   }
 }
 
 export default App;
-
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,23 +1,103 @@
-import React, { Component } from 'react';
-import Social from './SocialMedia';
+import React, { Component } from "react";
+import Social from "./SocialMedia";
+import octocat from "../img/octocat.png";
 
 class Landing extends Component {
-  render () {
+  state = {
+    events: []
+  };
+
+  componentDidMount() {
+    try {
+      const events = [
+        {
+          id: "event1",
+          eventTitle: "Datapath Website",
+          eventSkills: "Mobile Design, CSS Flexbox, CSS Grid",
+          img: octocat,
+          eventDescription:
+            "Modern CSS Flexbox, Mobile Styles, Cross Browser Testing and custom coded Interfaces with JavaScript",
+          eventUrl: "https://www.mydatapath.com/"
+        },
+        {
+          id: "event2",
+          eventTitle: "Kafka Streams and Events in a Web App",
+          eventSkills:
+            "Bloging, Technical Documentation, Enterprise Software Design",
+          img: octocat,
+          eventDescription:
+            "A write up/ tutorial of a demo I created to show how Kafka's message streams could be used in a modern web app. Part 1 of 3",
+          eventUrl:
+            "https://dev.to/jbull328/wash-away-the-crud-with-kafka-streaming-3lik"
+        },
+        {
+          id: "event3",
+          eventTitle: "Kafka Streams and Events in a Web App",
+          eventSkills:
+            "Bloging, Technical Documentation, Enterprise Software Design",
+          img: octocat,
+          eventDescription:
+            "Modern CSS Flexbox, Mobile Styles, Cross Browser Testing and custom coded Interfaces with JavaScript",
+          eventUrl: "https://www.mydatapath.com/"
+        }
+      ];
+      this.setState({
+        events
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    const events = this.state;
     return (
       <div className="Landing">
-        <h3>Welcome to my portfolio site.</h3>
-        <p>• 4+ years professional experience with JavaScript technology on both Front End and Back End web application design and development.
-        <br/>• Proficiency in developing web applications with React UI, JQuery, Node, Express, AWS, API integrations, NoSQL Databases.
-        <br/>• In-depth experience in front-end web design and development using HTML5, CSS, Bootstrap, Javascript, jQuery, JSON, AJAX, React and Angular 2/4.
-        <br/>• Great team player with strong communication skills, problem-solving skills and ability to learn as well as adapting to new technology.
-        <br/>• Agile Methodology, with remote teams or in-house team
-            <br/>I am a family man, I have two great kids and I love spending as much time with them as I can.<br/> I love creating things outside of code and it fules my fire, to cook, brew my own beer, and contribute in my community.<br/>
-            <br/>Thanks, and enjoy.<br/>
+        <h3>A develeper with skills all and experience.</h3>
+        <p>
+          • 4+ years professional experience with Software Engineering on both
+          Front End and Back End web application design and development.
+          <br />• Proficiency in developing web applications with React UI,
+          Vue.js JQuery, Node, Express, AWS, API Design and integration, NoSQL
+          Databases(MongoDB) and SQL Databases.
+          <br />• In-depth experience in front-end web development and
+          development using HTML5, CSS, Bootstrap, Javascript, jQuery, JSON,
+          AJAX, React and Angular 2/4.
+          <br />• Great team player with strong communication skills,
+          problem-solving skills and ability to learn as well as adapting to new
+          technology.
+          <br />• Agile Methodology, with remote teams or in-house team
+          <br />I am a family man, I have two great kids and I love spending as
+          much time with them as I can.
+          <br /> I love creating things outside of code and it fules my fire, to
+          cook, brew my own beer, and contribute in my community.
+          <br />
+          <br />
+          Thanks, and enjoy.
+          <br />
           John
         </p>
+        <div className="currentEvents">
+          {this.state.events.map(events => {
+            return (
+              <a href={events.eventUrl}>
+                <div className="eventCard container" key={events.id}>
+                  <img className="eventImg" src={events.img} />
+                  <article>
+                    <h3 className="eventTitle">{events.eventTitle}</h3>
+                    <p className="eventDescription">
+                      {events.eventDescription}
+                    </p>
+                    <p className="eventSkills">{events.eventSkills}</p>
+                  </article>
+                </div>
+              </a>
+            );
+          })}
+        </div>
         <Social />
       </div>
-    )
+    );
   }
 }
 
